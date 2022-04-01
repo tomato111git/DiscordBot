e34ef5cb83ca462650f9e41f292d79be1bb785b4
tomato111 <eidaitanaka10@gmail.com>
2022-04-01 17:22:06 +0900

    second commit

--- bot.py
+++ bot.py
@@ -21,6 +21,18 @@ async def on_message(message):
     if message.channel.name == 'bot-working-check':
         if message.content.startswith('$hello'):
             await message.channel.send('Hello!')
+    
+    bad_words = ['bad', "stop", "unfriendly"]
+    for word in bad_words:
+        if message.content.count(word) > 0:
+            print("Bad word deleted")
+            await message.channel.purge(limit=1)
+
+@client.event
+async def on_member_join(member):
+    for channel in member.guild.channels:
+        if str(channel) == 'general':
+            await channel.send_message(f"""Welcometo the server{member.mention}""")
         
 
 
